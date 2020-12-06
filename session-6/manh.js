ad1fc51e6bc11427a61f83d7b61543aa89e86fd8
DESKTOP-BRBI4NR\Manh20cm <manhducvuhaha69@gmail.com>
2020-12-06 12:27:38 +0700

    btvn-7

---
+++ session-6/manh.js
@@ -0,0 +1,88 @@
+
+
+
+
+// // // var manh=[1,2,3,4,5,6,7];
+// // // //var anh = manh.map(x => x*5);
+// // // var ha=manh.map(functionM);
+// // // function functionM(value,index,array){
+// // //     return value >4 ;
+// // // }
+// // // alert(functionM);
+
+
+
+// // confirm("bạn có chắc muốn xóa không");
+
+// // // manh.slice(2.1);
+
+// // var soA=[1,2,3,4,5,6];
+// // if(soA[4]<5){
+// //     alert("sai");
+// // }
+// // else {
+// //     alert("so sánh đúng ");
+// // }
+// // var x=2;
+// // switch(x){
+// //      case 1 : if(x>15){
+// //          alert("sai");
+// //          break;
+// //      }
+
+// //      case 2 : if(x<10){
+// //          alert("anh mạnh tb");
+// //      }
+// // }
+
+
+
+//  d=()=>(
+//     var b = document.getElementById("lname").value;
+//   var c=document.getElementById("fname").value=b;
+//   if(b.length>10){
+//     document.getElementById("dis").innerHTML="nhập quá kí tự ";
+//   }
+// // )
+// function sayHello() {
+//   alert('Hello');
+
+
+manh = () => {
+  var b = document.getElementById("email2").value;
+
+
+  switch (b) {
+
+    case '1': {
+      window.open(' https://quantrimang.com')
+      break;
+    }
+
+    case '2': {
+      window.open('https://download.com.vn')
+      break;
+    }
+
+    case '3': {
+      window.open(' https://vndoc.com')
+      break;
+    }
+
+    default: {
+      window.open(' https://meta.vn')
+
+    }
+  }
+
+
+}
+
+
+// }
+// setTimeout(sayHello, 3000);
+// function manh(){
+//   for(let i=0;i<10;i++){
+//     alert(i)
+//   }
+// }
